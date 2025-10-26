4b18c426fc9d7dfaab8cf5b469b87e2d1b1018e6
SHREYASPATIL2005 <159605502+SHREYASPATIL2005@users.noreply.github.com>
2025-10-26 22:04:10 +0530

    Java added

---
+++ Test.java
@@ -0,0 +1,10 @@
+public class Test {
+
+       public static void main(string[] args) {
+         System.out.println(**************************************************************);
+         System.out.println("Hello from GitHub !");
+         System.out.println(**************************************************************);
+
+       }
+}
+         
